428ba49a7aad8c2f6a3fd980f5556d030c5387ea
innerproduct <innerproduct@users.noreply.github.com>
2020-10-11 18:51:29 +0800

    PNG is a better format?

--- write_images.py
+++ write_images.py
@@ -7,6 +7,9 @@ from pdf2image.exceptions import (
     PDFPageCountError,
     PDFSyntaxError
 )
+
+outdir = './imgs/png/'
+
 pdfs = [s for s in os.listdir('.') if 'pdf' in s]
 
 for pdf in sorted(pdfs):
@@ -14,7 +17,7 @@ for pdf in sorted(pdfs):
   with tempfile.TemporaryDirectory() as path:
     images_from_path = convert_from_path(pdf, output_folder=path, fmt='jpeg')
     for i,image in enumerate(images_from_path):
-      outfilename = pdf.replace('.pdf','_'+str(i)+'.bmp') 
-      image.save(outfilename)
+      outfilename = pdf.replace('.pdf','_'+str(i)+'.png') 
+      image.save(os.path.join(outdir, outfilename))
       print(outfilename, 'saved!')
 
